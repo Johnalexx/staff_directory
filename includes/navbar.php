125ba6c5e4d9c439f50c3bf5b06738625dc3ad0c
anchor <anchor@localhost.localdomain>
2020-07-30 16:02:57 +0100

    Includes

---
+++ includes/navbar.php
@@ -0,0 +1,48 @@
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <title>Dashboard</title>
+        <meta content="Responsive admin theme build on top of Bootstrap 4" name="description" />
+        <meta content="Themesbrand" name="author" />
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!--calendar css-->
+        <link href="../plugins/fullcalendar/css/fullcalendar.min.css" rel="stylesheet">
+
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/metismenu.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/john.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body class="left-side-menu-light">
+     <!-- Begin page -->
+     <div id="wrapper">
+        <!-- Top Bar Start -->
+      <div class="topbar">
+          <!-- LOGO -->
+          <div class="topbar-left">
+              <a class="logo">
+                  <span class="logo-light">
+                      <img src="assets/images/logo-light.png" alt="" height="70">
+                  </span>
+                  <span class="logo-sm">
+                      <img src="assets/images/logo-sm.png" alt height="22">
+                  </span>
+               </a>
+       
+           </div>
+           <ul class="list-inline menu-left mb-5">
+                  <li class="float-left">
+                      <button class="button-menu-mobile open-left waves-effect">
+                          <i class="mdi mdi-menu">
+                     
+                          </i>
+                   </button>
+          </li>
+            </ul>
+       </div>
